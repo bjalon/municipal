6080d66601031d338dece055f24821cf385b017b
Benjamin JALON <bjalon@qastia.com>
2020-05-07 01:40:28 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 hugo
-rsync -az public/ root@51.178.53.161:/var/www/municipales/html/
 
+rsync -az public/ root@51.178.53.161:/var/www/municipales/html/
+rsync -az intro/ root@51.178.53.161:/var/www/default/html/
